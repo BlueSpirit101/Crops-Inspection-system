cfed1b80f619f286909226ba0363551999ebfccb
Prakash Thapa <bluestar5pt4488@gmail.com>
2026-01-09 00:55:33 +0545

    react_initial_project

---
+++ crop-insp/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>HOME PAGE
+      <h1>Welcome to my  CROP FIELD...</h1>
+      <p>All the Hostages!.. Board towards my CROP FIELD!!.. hehahahaaa</p>
+      <p>Master ROSI... should'nt it be the supporters instead of Hostages?!!..</p>
+      <p>General IRO.. They aren't supporters but my best friends and so am I.. Also I'm not a bad guy to express rheorically... </p>
+    </div>
+    
+  )
+}
+
+export default Home
